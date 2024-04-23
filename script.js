02f6b37b6e23efce39f47f032b173aea313d8df7
fff122 <159777134+fff122@users.noreply.github.com>
2024-04-23 23:42:28 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+let navbarItem = document.querySelectorAll('.item')
+for (let i = 0; i < navbarItem.length; i++) {
+    navbarItem[i].addEventListener('click', function () {
+        for (let i = 0; i < navbarItem.length; i++) {
+            navbarItem[i].classList.remove('active')
+        }
+        this.classList.add('active')
+    })
+}
+
+var typed = new Typed(".multiple-text", {
+    strings: ['Reward brother', "Dog"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
+
